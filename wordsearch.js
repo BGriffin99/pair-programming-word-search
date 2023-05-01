6163747d9411bf5cabe848cb90de79b41a4a0e92
Brady Griffin <bradyklg@gmail.com>
2023-05-01 12:17:28 -0400

    completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  if (!letters || letters.length === 0) {
+    return false; // handle empty input array
+  }
+
+  const numRows = letters.length;
+  const numCols = letters[0].length;
+
+  // check for horizontal occurrence
+  for (let i = 0; i < numRows; i++) {
+    const row = letters[i].join('');
+    if (row.includes(word)) {
+      return true;
     }
+  }
+
+  // check for vertical occurrence
+  for (let j = 0; j < numCols; j++) {
+    let col = '';
+    for (let i = 0; i < numRows; i++) {
+      col += letters[i][j];
+    }
+    if (col.includes(word)) {
+      return true;
+    }
+  }
+
+  return false; // word not found
 }
 
 module.exports = wordSearch
